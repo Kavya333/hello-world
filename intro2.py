1eecc2112f48f48bb841dfb646c5c4c838a9ac68
Kavya333 <kavichandru333@gmail.com>
2017-05-05 12:35:56 +0530

    Add files via upload

---
+++ intro2.py
@@ -0,0 +1,16 @@
+#function
+def power(p,q):
+ return p**q
+def greater(p,q):
+    if p>q :
+     return 3
+def modulo(p,q):
+ return p%q
+def divide(p,q):
+ return float(p)/float(q)
+a=3
+b=2
+print (power(a,b))
+print (greater(a,b))
+print (modulo(a,b))
+print (float(divide(a,b)))
---
+++ intro3.py
@@ -0,0 +1,7 @@
+#lists
+names=['darwin','krishna','dickens']
+names[2]="bharathiar"
+number=[1,2,3,4]
+print (names[0],names[2],number[2:])
+print (names)
+print ([names]*number[1])
